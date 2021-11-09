2675c362d60a5454dd852398b37ea10559c7a58d
reinardricky <reinardricky@gmail.com>
2021-11-09 12:26:17 +0700

    Update drawer.dart

--- lib/Screens/drawer.dart
+++ lib/Screens/drawer.dart
@@ -1,17 +1,177 @@
 import 'package:flutter/material.dart';
-import 'package:mobileapp/Variables/global.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:mobileapp/Screens/login.dart';
 import 'dart:ui' show ImageFilter;
 
-class AppDrawer {
-  static get widget => Stack(children: [
-        Drawer(
-            child: Column(
-          children: [
+class AppDrawer extends StatelessWidget {
+  final padding = EdgeInsets.symmetric(horizontal: 20);
+  @override
+  Widget build(BuildContext context) {
+    final name = 'Sarah Abs';
+    final email = 'sarah@abs.com';
+    final urlImage =
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80';
+
+    return Drawer(
+      child: Material(
+        color: Colors.yellow[200],
+        child: ListView(
+          children: <Widget>[
             Container(
-              color: Global.colors.yellow,
-              height: 200,
-            )
+              padding: padding,
+              child: Column(
+                children: [
+                  const SizedBox(height: 12),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                        'UI Mobile-Staff',
+                      style:TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    height:1,
+                    thickness: 1,
+                  ),
+                  const SizedBox(height: 24),
+                  buildMenuItem(
+                    text: 'People',
+                    icon: Icons.people,
+                    onClicked: () => selectedItem(context, 0),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'Favourites',
+                    icon: Icons.favorite_border,
+                    onClicked: () => selectedItem(context, 1),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'Workflow',
+                    icon: Icons.workspaces_outline,
+                    onClicked: () => selectedItem(context, 2),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'Updates',
+                    icon: Icons.update,
+                    onClicked: () => selectedItem(context, 3),
+                  ),
+                  const SizedBox(height: 24),
+                  Divider(color: Colors.white70),
+                  const SizedBox(height: 24),
+                  buildMenuItem(
+                    text: 'Notifikasi',
+                    icon: Icons.notifications_outlined,
+                    onClicked: () => selectedItem(context, 4),
+                  ),
+                  const SizedBox(height: 16),
+                  buildMenuItem(
+                    text: 'Keluar',
+                    icon: Icons.logout,
+                    onClicked: () => selectedItem(context, 5),
+                  ),
+                ],
+              ),
+            ),
           ],
-        ))
-      ]);
+        ),
+      ),
+    );
+  }
+
+  Widget buildHeader({
+    required String urlImage,
+    required String name,
+    required String email,
+    required VoidCallback onClicked,
+  }) =>
+      InkWell(
+        onTap: onClicked,
+        child: Container(
+          padding: padding.add(EdgeInsets.symmetric(vertical: 40)),
+          child: Row(
+            children: [
+              CircleAvatar(radius: 30, backgroundImage: NetworkImage(urlImage)),
+              SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    email,
+                    style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                ],
+              ),
+              Spacer(),
+              CircleAvatar(
+                radius: 24,
+                backgroundColor: Color.fromRGBO(30, 60, 168, 1),
+                child: Icon(Icons.add_comment_outlined, color: Colors.black),
+              )
+            ],
+          ),
+        ),
+      );
+
+  Widget buildSearchField() {
+    final color = Colors.black;
+
+    return TextField(
+      style: TextStyle(color: color),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        hintText: 'Search',
+        hintStyle: TextStyle(color: color),
+        prefixIcon: Icon(Icons.search, color: color),
+        filled: true,
+        fillColor: Colors.white12,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: color.withOpacity(0.7)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: color.withOpacity(0.7)),
+        ),
+      ),
+    );
+  }
+
+  Widget buildMenuItem({
+    required String text,
+    required IconData icon,
+    VoidCallback? onClicked,
+  }) {
+    final color = Colors.black;
+    final hoverColor = Colors.black12;
+
+    return ListTile(
+      leading: Icon(icon, color: color),
+      title: Text(text, style: TextStyle(color: color)),
+      hoverColor: hoverColor,
+      onTap: onClicked,
+    );
+  }
+
+  void selectedItem(BuildContext context, int index) {
+    Navigator.of(context).pop();
+
+    switch (index) {
+      case 5:
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ));
+        break;
+    }
+  }
 }
