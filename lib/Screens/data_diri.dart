d6ba702b9f1d170013aef84e8dbbf68f46be2e85
reinardricky <reinardricky@gmail.com>
2022-01-06 13:07:44 +0700

    tambah bagian absensi data diri uipay

---
+++ lib/Screens/data_diri.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DataDiri extends StatefulWidget {
+  const DataDiri({Key? key}) : super(key: key);
+
+  @override
+  _DataDiriState createState() => _DataDiriState();
+}
+
+class _DataDiriState extends State<DataDiri> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
---
+++ lib/Screens/ui_pay.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UIPay extends StatefulWidget {
+  const UIPay({Key? key}) : super(key: key);
+
+  @override
+  _UIPayState createState() => _UIPayState();
+}
+
+class _UIPayState extends State<UIPay> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
